4fb3c23d77ac959471269230450e7d64e9c12a06
Ryan Hillis <ryanbhillis@gmail.com>
2025-05-14 09:30:45 -0400

    sr events

--- ChatHub.cs
+++ ChatHub.cs
@@ -2,12 +2,26 @@ using Microsoft.AspNetCore.SignalR;
 
 public class ChatHub : Hub
 {
+
+    public string playerList = "";
+    public int playerCount = 0;
     public async Task SendMessage(string message) {
         await Clients.All.SendAsync("ReceiveMessage", "You said: " + message);
     }
 
+    public async Task JoinPlayer(string name) {
+        playerList += ", " + name;
+        playerCount += 1;
+
+        await Clients.All.SendAsync("PlayerList", playerCount + " Players: " + playerList);
+    }
+
     public async Task CreateLobby(string lobby) {
         await Clients.All.SendAsync("LobbyCreated", "Join Code: " + lobby);
+    }
+
+    public async Task FindPlayers() {
+        await Clients.Caller.SendAsync("PlayerList", playerCount + " Players: " + playerList);
     }
 
     //clients:
